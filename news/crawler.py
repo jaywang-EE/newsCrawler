4f78de33bc82b8b455c4c7511658dc730cf91b58
jaywang-EE <gaaiia37896@gmail.com>
2020-02-25 00:21:07 -0500

    Update crawler.py

--- news/crawler.py
+++ news/crawler.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from datetime import datetime
+import random
 
-URL = "https://www.nps.gov"
-HEADERS = {'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'),
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
-
+HEADERS = ["Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0"
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)"]
 
 class Crawler:
     base_url = ""
@@ -15,7 +17,7 @@ class Crawler:
         pass
 
     def search(self, params):
-        response = requests.get(self.base_url+self.search_addr, params=params, headers=HEADERS)
+        response = requests.get(self.base_url+self.search_addr, params=params, headers={'User-Agent':random.choice(HEADERS)})
         return BeautifulSoup(response.text, 'html.parser')
 
 class NTCrawler(Crawler):
