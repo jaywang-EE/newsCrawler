710bf59445922f3d947e017677b286edc373df66
jaywang-EE <gaaiia37896@gmail.com>
2020-02-25 10:54:55 -0500

    add icon

--- newsCrawler/urls.py
+++ newsCrawler/urls.py
@@ -9,3 +9,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Keep
     path('news/', include('news.urls')),  # Keep
 ]
+import os
+from django.views.static import serve
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'static'),
+        }
+    ),
+]
